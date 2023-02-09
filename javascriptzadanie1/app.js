1d74d5d89d6d8bf2a4060bc344bd0ce56b7c7010
AdrianCzy <120756469+AdrianCzy@users.noreply.github.com>
2023-02-09 13:48:54 +0100

    Add files via upload

---
+++ javascriptzadanie1/app.js
@@ -0,0 +1,45 @@
+//zadanie1
+const liczba_1 = parseInt(prompt('Podaj Liczbę: '))
+alert(liczba_1)
+//zadanie2
+const liczba_2 = parseInt(prompt('Podaj Liczbę: '))
+alert(liczba_2 * -1)
+//zadanie3
+const liczba_3 = parseInt(prompt('Podaj Liczbę: '))
+alert(liczba_3 * 15)
+//zadanie4
+const liczba_4 = parseInt(prompt('Podaj Liczbę: '))
+alert(liczba_4 * 0.5)
+//zadanie5
+const liczba_5 = parseInt(prompt('Podaj Liczbę: '))
+alert(Math.pow(liczba_5, 2))
+//zadanie6
+const liczba_6 = parseInt(prompt('Podaj Liczbę: '))
+alert(liczba_6 /2)
+//zadanie7
+const liczba_7 = parseInt(prompt('Podaj Liczbę: '))
+alert(liczba_7 % 2)
+//zadnie8
+const liczba_8 = parseInt(prompt('Podaj Liczbę: '))
+alert(liczba_8 + 1)
+//zadanie9
+const liczba_9 = parseInt(prompt('Podaj Liczbę: '))
+alert(liczba_9 - 1)
+//zadanie10
+const liczba_10 = parseInt(prompt('Podaj Liczbę: '))
+alert(Math.floor(liczba_10 /3))
+//zadanie11
+const liczba_11 = parseInt(prompt('Podaj Liczbę: '))
+alert(Math.ceil(liczba_10 /3))
+//zadanie12
+const liczba_12 = parseInt(prompt('Podaj Liczbę: '))
+alert(Math.round(liczba_10 /3))
+//zadanie13
+const liczba_13 = parseInt(prompt('Podaj Liczbę: '))
+alert(Math.sqrt(liczba_13))
+//zadanie14
+const liczba_14 = parseInt(prompt('Podaj Liczbę: '))
+alert(liczba_14 * Math.floor(Math.random()*50+0))
+//zadanie15
+const liczba_15 = parseInt(prompt('Podaj Liczbę: '))
+alert(Math.abs(liczba_15))
